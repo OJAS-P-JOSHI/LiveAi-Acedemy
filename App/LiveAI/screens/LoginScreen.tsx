d62ac08deb84142612c7d67541023ef610676527
Sakshi Tilkari <sakshitilkari3@gmail.com>
2024-09-13 10:30:28 +0530

    App.tsx setup

---
+++ App/LiveAI/screens/LoginScreen.tsx
@@ -0,0 +1,21 @@
+import { View, Text, StyleSheet } from "react-native";
+
+const Login = () => {
+    return(
+        <View style={styles.container}>
+        <Text>Open up Login.tsx to start working on your app!</Text>
+        
+      </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  
+export default Login;
